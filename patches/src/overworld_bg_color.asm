05f52ce3fb37be2b1d7c0f25b018de4806fba592
Brent Kerby <blkerby@gmail.com>
2026-08-23 17:41:12 -0600

    disable color math outside iris, keep backdrop in interior

--- patches/src/overworld_bg_color.asm
+++ patches/src/overworld_bg_color.asm
@@ -7,6 +7,8 @@ incsrc "symbols.inc"
 !free_space_bank_a0_end = $A0A620
 !ThemeBackgroundColors = $A09E00
 !AreaBackgroundSettingsOffset = 24
+!GeneratedBG1Enabled = $BFE406
+!BackgroundSettings = $7ECC72
 
 org $8CFF91
 hook_SetOverworldBackgroundColor:
@@ -28,8 +30,20 @@ org $8BFEB5
 hook_SetOverworldFixedColorAndScrollBackdrop:
     JML SetThemeOverworldFixedColorAndScrollBackdrop
 assert pc() == $8BFEB9
-org $8BFEC6
-return_SetOverworldFixedColorAndScrollBackdrop:
+
+org $8BFF0B
+return_SetOverworldFixedColorWithoutSubscreen:
+
+org $80F2D7
+hook_PreserveGeneratedBackdropDuringSpotlight:
+    JML PreserveGeneratedBackdropDuringSpotlight
+assert pc() == $80F2DB
+
+org $829999
+hook_FinishGeneratedSpotlight:
+    JML FinishGeneratedSpotlight
+    NOP
+assert pc() == $82999E
 
 org !free_space_bank_a0_start
 
@@ -68,6 +82,51 @@ SetThemeOverworldBackgroundColorCacheOnly:
 
 .vanilla
     JML return_SetOverworldBackgroundColorCacheOnly
+
+PreserveGeneratedBackdropDuringSpotlight:
+    ; Run hi-jacked instructions:
+    LDA.b $1B
+    BNE .indoors
+
+    LDA.l !GeneratedBG1Enabled
+    BEQ .black
+    LDA.l !BackgroundSettings
+    CMP.b #$02
+    BNE .black
+
+    LDA.b #$A2                ; Clip and disable color math outside the iris.
+    STA.b $99
+    JML $80F2E7
+
+.indoors
+    JML $80F2E7
+
+.black
+    JML $80F2DB
+
+FinishGeneratedSpotlight:
+    LDA.b $1B
+    BNE .vanilla
+    LDA.l !GeneratedBG1Enabled
+    BEQ .vanilla
+    LDA.l !BackgroundSettings
+    CMP.b #$FF
+    BEQ .vanilla
+
+    CMP.b #$02
+    BNE .generated
+    LDA.b #$82                ; Restore ordinary overworld color math.
+    STA.b $99
+
+.generated
+    JML $8299CB
+
+.vanilla
+    ; Run hi-jacked instructions:
+    REP #$30
+    LDX.w #$4C26
+
+    JML $82999E
 
 SetThemeOverworldFixedColorAndScrollBackdrop:
     TXA                         ; Run hi-jacked instruction
@@ -110,15 +169,17 @@ SetThemeOverworldFixedColorAndScrollBackdrop:
     STA.b $02
     PLA
     STA.b $00
-    PLY
     CPX.w #$0002
     BEQ .backdrop
+    CPX.w #$0001             ; Carry set selects the half-add subscreen.
 
+    PLY
     PLA
     PLX
     BRA .store
 
 .backdrop
+    PLY
     PLA
     PLX
     TAX
@@ -161,13 +222,26 @@ SetThemeOverworldFixedColorAndScrollBackdrop:
 .vanilla
     PLA
     TAX
+    TXA
+    STA.l $7EC500
+    JML $8BFEBA
 
 .store
     STA.l $7EC500
     STA.l $7EC300
     STA.l $7EC540
     STA.l $7EC340
-    JML return_SetOverworldFixedColorAndScrollBackdrop
+
+    LDA.w #$4020
+    STA.b $9C
+    LDA.w #$8040
+    STA.b $9D
+
+    BCS .enable_subscreen
+    JML return_SetOverworldFixedColorWithoutSubscreen
+
+.enable_subscreen
+    JML $8BFF9D
 
 assert pc() <= !free_space_bank_a0_end
 assert !ThemeBackgroundColors+$0140 <= !free_space_bank_a0_start
